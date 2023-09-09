332d9481c4ff7c8d3216c6f502a1bb70fcc836d4
Paul Glazachev <glazachev9777@gmail.com>
2023-09-09 17:58:24 +0300

    Добавлен сервіс статистики

---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,92 @@
+import Foundation
+
+protocol StatisticService{
+    var totalAccuracy: Double { get }
+    var gamesCount: Int { get }
+    var bestGame: GameRecord { get }
+    
+    func store(correct count: Int, total amount: Int)
+}
+
+final class StatisticServiceImplementation: StatisticService{
+    
+    private let userDefaults = UserDefaults.standard
+    
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    var totalAccuracy: Double{
+        get {
+            let data = userDefaults.double(forKey: Keys.total.rawValue)
+            return data
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.total.rawValue)
+        }
+    }
+    
+    var gamesCount: Int {
+        get {
+            let data = userDefaults.integer(forKey: Keys.gamesCount.rawValue)
+            return data
+        }
+        set {
+            userDefaults.set(newValue, forKey: Keys.gamesCount.rawValue)
+        }
+        
+    }
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+                let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            
+            return record
+        }
+        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        
+        // best game
+        var newGame = GameRecord(correct: count, total: amount, date: Date())
+        if (newGame > bestGame) {
+            bestGame = newGame
+        }
+        
+        // games count
+        gamesCount = gamesCount + 1
+        
+        // total
+        var accuracy: Double = Double(amount) / 100 * Double(count)
+        if (totalAccuracy == 0){
+            totalAccuracy = accuracy
+        } else {
+            totalAccuracy = (totalAccuracy + accuracy) / 2
+        }
+    }
+    
+}
+
+struct GameRecord: Codable, Comparable {
+    static func < (lhs: GameRecord, rhs: GameRecord) -> Bool {
+        return lhs.correct < rhs.correct
+    }
+    
+    let correct: Int
+    let total: Int
+    let date: Date
+}
+
+
