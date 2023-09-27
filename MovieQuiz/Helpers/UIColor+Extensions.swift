54733982ea28e7be380c6ac1092497c76d12f6f9
Paul Glazachev <glazachev9777@gmail.com>
2023-09-27 20:15:54 +0300

    Исправлена ошибка некорректных цветов

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -2,6 +2,10 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static var ypGreen: UIColor { UIColor.green }
-    static var ypRed: UIColor { UIColor.red }
+    static var ypGreen = UIColor(named: "YP Green") ?? .green
+    static var ypRed = UIColor(named: "YP Red") ?? .red
+    static var ypWhite = UIColor(named: "YP White") ?? .white
+    static var ypGray = UIColor(named: "YP Gray") ?? .gray
+    static var ypBlack = UIColor(named: "YP Black") ?? .black
+    static var ypBackground = UIColor(named: "YP Background") ?? .gray
 }
